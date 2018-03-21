80ecfd4643954f46816a55d56548a9a4ac2e9eb7
drice82 <xujiabin82@gmail.com>
2018-03-21 09:53:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ if [ "${1:0:1}" = '-' ]; then
 fi
    
 sed -ri "s/mysqlhost/$MYSQL_HOST/g" /shadowsocksr/usermysql.json
+sed -ri "s/mysqlport/$MYSQL_PORT/g" /shadowsocksr/usermysql.json
+sed -ri "s/mysqluser/$MYSQL_USER/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqlpwd/$MYSQL_PASSWORD/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqldb/$MYSQL_DBNAME/g" /shadowsocksr/usermysql.json
   
