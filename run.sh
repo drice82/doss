9dc5ee17aca5f734ef55001a66c5c6f2f93fe5ae
drice82 <xujiabin82@gmail.com>
2018-03-21 10:01:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,5 +13,6 @@ sed -ri "s/mysqlport/$MYSQL_PORT/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqluser/$MYSQL_USER/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqlpwd/$MYSQL_PASSWORD/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqldb/$MYSQL_DBNAME/g" /shadowsocksr/usermysql.json
+sed -ri "s/setmul/$SETMUL/g" /shadowsocksr/usermysql.json
   
 exec python /shadowsocksr/server.py
