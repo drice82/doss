65003223466c43931f5867586022ad9f21d73dda
drice82 <xujiabin82@gmail.com>
2019-01-04 16:06:40 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ sed -ri "s/mysqluser/$MYSQL_USER/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqlpwd/$MYSQL_PASSWORD/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqldb/$MYSQL_DBNAME/g" /shadowsocksr/usermysql.json
 sed -ri "s/setmul/$SETMUL/g" /shadowsocksr/usermysql.json
-
+python /shadowsocksr/server.py
