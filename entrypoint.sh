e167e172c313d43320e036a60a35a8d02bd7f950
drice82 <xujiabin82@gmail.com>
2019-01-04 15:57:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-
-PATH=/bin:/sbin:$PATH
- 
-set -e
- 
-if [ "${1:0:1}" = '-' ]; then
-    set -- python "$@"
-fi
-   
+  
 sed -ri "s/mysqlhost/$MYSQL_HOST/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqlport/$MYSQL_PORT/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqluser/$MYSQL_USER/g" /shadowsocksr/usermysql.json
@@ -15,4 +7,4 @@ sed -ri "s/mysqlpwd/$MYSQL_PASSWORD/g" /shadowsocksr/usermysql.json
 sed -ri "s/mysqldb/$MYSQL_DBNAME/g" /shadowsocksr/usermysql.json
 sed -ri "s/setmul/$SETMUL/g" /shadowsocksr/usermysql.json
   
-exec "$@"
+python /shadowsocksr/server.py
