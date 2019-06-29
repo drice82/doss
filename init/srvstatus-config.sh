d1b64dfec5286e4c9777a5de879b20c063cbb962
drice82 <xujiabin82@gmail.com>
2019-06-29 16:33:00 +0800

    Update srvstatus-config.sh

--- init/srvstatus-config.sh
+++ init/srvstatus-config.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+sed -ri "s/SETUP_USERNAME/$STATUS_USER/g" /serverstatus/client-linux.py
+sed -ri "s/SETUP_SERVER_ADDRESS/$STATUS_ADDRESS/g" /serverstatus/client-linux.py
 sed -ri "s/SETUP_USERNAME/$STATUS_USER/g" /serverstatus/client-psutil.py
 sed -ri "s/SETUP_SERVER_ADDRESS/$STATUS_ADDRESS/g" /serverstatus/client-psutil.py
